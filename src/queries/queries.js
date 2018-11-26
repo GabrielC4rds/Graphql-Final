726b2708aa748395ea0b7f23a9730dbb05758e1b
GabrielC4rds <43912656+GabrielC4rds@users.noreply.github.com>
2018-11-26 10:36:35 -0200

    Add files via upload

---
+++ src/queries/queries.js
@@ -0,0 +1,52 @@
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo';
+
+
+const getBooksQuery=gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
+
+const getAuthorsQuery=gql`
+  {
+    authors{
+      name
+      id
+    }
+  }
+`
+
+const addBookMutation=gql`
+  mutation($name:String!,$genre:String!,$authorID:ID!){
+    addBook(name:$name,genre:$genre,authorID:$authorID){
+      name
+      id
+    }
+  }
+`
+
+const getBookQuery=gql`
+  query($id: ID){
+    book(id:$id){
+      id
+      name
+      genre
+      author{
+        id
+        name
+        age
+        books{
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
+
+export{getAuthorsQuery,getBooksQuery,addBookMutation,getBookQuery};
